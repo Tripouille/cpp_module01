8164ab2b5c6d479879c6b938c3c6423b1c68fc0c
jgambard <jgambard@student.42lyon.fr>
2020-07-21 16:45:28 +0200

    Add missing header

--- ex08/main.cpp
+++ ex08/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgambard <jgambard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/21 16:44:26 by jgambard          #+#    #+#             */
+/*   Updated: 2020/07/21 16:44:27 by jgambard         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Human.hpp"
 
 int main(void)
@@ -6,7 +18,7 @@ int main(void)
 
 	bob.action("Coucou", "42");
 	bob.action("meleeAttack", "42");
-	bob.action("rangedAttack", "42");
+	bob.action("rangedAttack", "42"6;
 	bob.action("intimidatingShout", "42");
 	bob.action("Bye", "42");
 	return (0);
